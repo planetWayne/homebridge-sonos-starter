e319d498c824cf6cf96ace081dbba2b5cbaa32c0
planetWayne <2609566+planetWayne@users.noreply.github.com>
2020-11-18 22:28:13 +0000

    version no's!

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function SonosAccessory(log, config) {
         this.preset = config["preset"];
 
         this.trackURI = config["trackURI"];
-        this.log("Track Enhanced v1.0.10");
+        this.log("Track Enhanced v1.0.11");
         this.log("Track URI Found = " + this.trackURI);
 
         if (!this.apiBaseUrl) throw new Error("You must provide a config value for 'apiBaseUrl'.");
